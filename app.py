7e32991aab23ab757602381ef67a6b54e1a6279b
elyonmendes-netizen <elyonmendes@gmail.com>
2026-04-07 19:58:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,34 @@
 import streamlit as st
+from questions import questions
+from scoring import calcular
 
-st.title("Teste funcionando")
-st.write("Se você está vendo isso, o app está OK ✅")
+st.title("Avaliação Psicossocial - COPSOQ (NR-1)")
+
+nome = st.text_input("Nome")
+email = st.text_input("E-mail")
+empresa = st.text_input("Empresa")
+
+st.write("Responda o questionário:")
+
+respostas = {}
+
+for q in questions:
+    resp = st.slider(q["text"], 1, 5, 3)
+
+    dim = q["dimension"]
+
+    if dim not in respostas:
+        respostas[dim] = []
+
+    respostas[dim].append(resp)
+
+if st.button("Gerar Resultado"):
+    resultado = calcular(respostas)
+
+    st.subheader("Resultado")
+
+    for dim, dados in resultado.items():
+        st.write(f"**{dim}**")
+        st.write(f"Média: {dados['media']}")
+        st.write(f"Classificação: {dados['classificacao']}")
+        st.write("---")
