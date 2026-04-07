3bbdd8591dabed2222beb3585f2159ad6e287f41
elyonmendes-netizen <elyonmendes@gmail.com>
2026-04-07 20:05:11 -0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,1 +1,7 @@
-
+questions = [
+    {"text": "Você trabalha sob pressão de tempo?", "dimension": "Demanda"},
+    {"text": "Seu trabalho exige muita concentração?", "dimension": "Demanda"},
+    {"text": "Você pode influenciar decisões?", "dimension": "Controle"},
+    {"text": "Você recebe apoio do seu superior?", "dimension": "Apoio"},
+    {"text": "Você se sente estressado?", "dimension": "Saúde"},
+]
